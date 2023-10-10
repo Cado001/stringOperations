a367a4f20df3a1df22b0ef5eb05b2b2af47fa85f
Micado Pierre <Biml16gb@gmail.com>
2023-10-10 12:15:30 -0400

    first stringOperation

---
+++ index.js
@@ -0,0 +1,34 @@
+let firstName = 'Cado'
+let age = 25
+let str = 'Hello JavaScript students!'
+let str2 = '      Hello!    '
+
+console.log(`Hello ${firstName}. You are ${age} today!`)
+console.log(str.length)
+
+
+//console.log(str.toUpperCase())
+
+//console.log(str.toLowerCase())
+
+//console.log(str.trim()) // Hello
+
+//console.log(str.replace()) 
+
+//str2.substring(8,9) // let up 
+
+let searchterm = 'Level'
+
+//str.startsWith(searchterm) // false
+
+//str.endsWith(searchterm) // true
+
+//str.includes(searchterm) // true
+
+//str.indexOf(searchterm) // 6
+
+let words = str.split('')
+// ['Hello', 'Level', 'Up', 'students!']
+
+
+
